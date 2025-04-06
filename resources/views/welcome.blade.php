254a8c5c5bf34c15e5f70834b8e9408f7e656a64
Alex <git.hub@ukr.net>
2025-04-06 20:43:48 +0200

    Laravel курс с нуля. Урок 5. Аутентификация. Обучение Ларавел

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Главная страница')
 
 @section('content')
-    {{-- @include('partials.header') --}}
+    @include('partials.header')
 
     <div class="grid grid-cols-1 md:grid-cols-3 gap-10 mt-10 mb-20">
         @foreach($posts as $post)
